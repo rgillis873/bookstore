4e4c1750ec2137e1b6af45460e6b2f44e7ccceb9
Robert Gillis <59716448+rgillis873@users.noreply.github.com>
2021-12-12 17:52:49 -0500

    Delete SQL directory

--- SQL/Indices.sql
+++
@@ -1,4 +0,0 @@
---Added to make sure addresses with null apt_num values are stil unique
-create unique index address_no_apt 
-on address (street_num_name, city, province, country, post_code) 
-where apt_num is null;
