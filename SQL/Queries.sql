7b9c8ca96d0eb19e04e02eb251d8fc00f972b45e
Rob Gillis <rgillis873@gmail.com>
2021-12-12 23:05:05 -0500

    Fix queries

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -1,11 +1,3 @@
---Used to fix the serial number sequences for author,publisher, review, address relations,
---as they were thrown off by the initial insertions into the db
-SELECT setval(pg_get_serial_sequence('author', 'auth_id'), MAX(auth_id)) FROM author;
-SELECT setval(pg_get_serial_sequence('publisher', 'pub_id'), MAX(pub_id)) FROM publisher;
-SELECT setval(pg_get_serial_sequence('review', 'review_id'), MAX(review_id)) FROM review;
-SELECT setval(pg_get_serial_sequence('address', 'add_id'), MAX(add_id)) FROM address;
-
-
 --Get list of book genres
 SELECT distinct genre 
 FROM book;
